41aec5faf69d427c353169ee65e6e36dbd71f3f1
Arkadius <humennyi.arkadii@gmail.com>
2017-12-16 18:10:54 +0200

    remove useless code

--- src/me/gumennyi/lab2/graphics/DefaultGraphics.java
+++ src/me/gumennyi/lab2/graphics/DefaultGraphics.java
@@ -28,20 +28,9 @@ public class DefaultGraphics implements me.gumennyi.lab2.graphics.Graphics {
 
     @Override
     public void drawPolygon(Polygon<Point2D> polygon, double lambert, BufferedImage image, Color lineColor) {
-//
         int r = (int) (200 * lambert);
-        graphics.setColor(new Color(polygon.isTop() ? r : 0, polygon.isTop() ? 0 : r, 0));
-//        Point2D a = polygon.getA();
-//        Point2D b = polygon.getB();
-//        Point2D c = polygon.getC();
-//        graphics.fillPolygon(new int[] {a.getX(), b.getX(), c.getX()},
-//                new int[] {a.getY(), b.getY(), c.getY()}, 3);
-//        if (lineColor != null) {
-//            graphics.setColor(lineColor);
-//            line(a,b);
-//            line(a,c);
-//            line(b,c);
-//        }
+//        graphics.setColor(new Color(polygon.isTop() ? r : 0, polygon.isTop() ? 0 : r, 0));
+
         SimpleMatrix polygonMatrix = getPolygonMatrix(polygon);
         Polygon<Point2D> rasterPolygon = getRasterPolygon(polygon, image);
         SimpleMatrix rasterMatrix = getPolygonMatrix(rasterPolygon);
@@ -49,12 +38,8 @@ public class DefaultGraphics implements me.gumennyi.lab2.graphics.Graphics {
         for (int x = (int) Utils.minX(polygon); x < Utils.maxX(polygon); x++) {
             for (int y = (int) Utils.minY(polygon); y < Utils.maxY(polygon); y++) {
                 if (Utils.containsPoint(polygon, x, y)) {
-//                    graphics.setColor(new Color(rgb));
-//                        graphics.drawRect(x, y, 1, 1);
-
                     SimpleMatrix rasterCoords = new SimpleMatrix(new double[][]{{x}, {y}, {1}});
                     SimpleMatrix result = transformMatrix.mult(rasterCoords);
-//                    System.out.println(result);
                     int x1 = (int) result.get(0, 0);
                     int y1 = (int) result.get(1, 0);
                     try {
@@ -71,34 +56,6 @@ public class DefaultGraphics implements me.gumennyi.lab2.graphics.Graphics {
                 }
             }
         }
-//        if (!polygon.isTop()) {
-////            System.out.println("_______________________");
-////            System.out.println(polygon);
-//            int yStart = rasterPolygon.getA().getY();
-//            int yEnd = rasterPolygon.getC().getY();
-//            int xStart = rasterPolygon.getA().getX();
-//            int xEnd = rasterPolygon.getC().getX();
-//            for (int i = yStart; i < yEnd; i++) {
-//                int i1 = xStart + ((xEnd - xStart) * i) / (yEnd - yStart);
-//                for (int j = xStart; j < i1; j++) {
-//                    SimpleMatrix rasterCoords = new SimpleMatrix(new double[][]{{j}, {i}, {1}});
-//                    SimpleMatrix result = transformMatrix.mult(rasterCoords);
-//
-//                    int x = (int) result.get(0, 0);
-//                    int y = (int) result.get(1, 0);
-//                    try {
-//                        int rgb = image.getRGB(j, i);
-//                        System.out.println(x + ' ' + y);
-//                        graphics.setColor(new Color(rgb));
-//                        graphics.drawRect(x, y, 1, 1);
-//                    } catch (Exception e) {
-//                        System.out.println(i + " " + j);
-//                    }
-////                    graphics.setColor(new Color((int) (200 * lambert), 0, 0));
-//                }
-//            }
-//        }
-
     }
 
     private Polygon<Point2D> getRasterPolygon(Polygon<Point2D> polygon, BufferedImage image) {
